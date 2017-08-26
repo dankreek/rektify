618488d4d8a3c4790ad0068166f483665d245589
Justin May <may.justin@gmail.com>
2017-08-26 15:05:21 -0700

    Version 0.0.4-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject dankreek/rektify "0.0.3"
+(defproject dankreek/rektify "0.0.4-SNAPSHOT"
   :description "ClojureScript library to manipulate any JS object model in a declarative way."
   :url "https://github.com/dankreek/rektify"
   :license {:name "Eclipse Public License"
