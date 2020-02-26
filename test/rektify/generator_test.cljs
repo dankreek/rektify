9c4390b4b615a832c68813d2c35fd8fe2305d3fa
Justin May <justin.may@cambiahealth.com>
2020-02-26 13:53:43 -0800

    ... simple generation and regeneration complete

--- test/rektify/generator_test.cljs
+++ test/rektify/generator_test.cljs
@@ -14,38 +14,41 @@
             (vt/node classes/blue-fish-desc)]))
 
 
-(deftest generator-state-functions
-  (is (thrown? js/Error (g/reset-state! {}))
-      "Error thrown when attempting to mutate state outside of generator lifecycle function")
+(deftest generator-state
+  (testing "state functions work as expected with mock state atom"
+    (is (thrown? js/Error (g/reset-state! {}))
+        "Error thrown when attempting to mutate state outside of generator lifecycle function")
 
-  ;; Manually binding an atom to *active-gen-state* to simulate the execution
-  ;; of a lifecycle function
-  (binding [g/*active-gen-state* (atom {:a "bee"})]
-    (g/swap-state! merge {:c "deezenutz"})
-    (is (= {:a "bee", :c "deezenutz"} @g/*active-gen-state*))
+    ;; Manually binding an atom to *active-gen-state* to simulate the execution
+    ;; of a lifecycle function
+    (binding [g/*active-gen-state* (atom {:a "bee"})]
+      (g/swap-state! merge {:c "deezenutz"})
+      (is (= {:a "bee", :c "deezenutz"} @g/*active-gen-state*))
 
-    (g/reset-state! {:oh-my "goodness!"})
-    (is (= {:oh-my "goodness!"} @g/*active-gen-state*))))
+      (g/reset-state! {:oh-my "goodness!"})
+      (is (= {:oh-my "goodness!"} @g/*active-gen-state*))))
+  (testing "state transitions between each lifecycle phase"
+
+    )
+  )
 
 
 (deftest generate
   (testing "single generator"
     (testing "generate a reified v-tree"
       (let [gen-props {:a "bee" :c "dee"}
-            expected-vals (atom {:props gen-props
-                                 :children [no-gen-v-tree]
-                                 :local-state {}})
+            expected-vals* (atom {:props gen-props
+                                  :local-state nil
+                                  :v-tree no-gen-v-tree})
             gen-desc {:generate
-                      (fn [props children local-state]
+                      (fn [props local-state]
                         (g/reset-state! {:new "state"})
-                        (is (= (:props @expected-vals) props)
+                        (is (= (:props @expected-vals*) props)
                             "correct props passed into generate function")
-                        (is (= (:children @expected-vals) children)
-                            "correct children passed into generate function")
-                        (is (= (:local-state @expected-vals) local-state)
+                        (is (= (:local-state @expected-vals*) local-state)
                             "correct local state passed into generate function")
-                        (first children))}
-            gen (g/generator gen-desc gen-props [no-gen-v-tree])
+                        (:v-tree @expected-vals*))}
+            gen (g/generator gen-desc gen-props)
             generated-gen (g/generate gen)
             orig-head& (g/obj& generated-gen)]
 
@@ -55,14 +58,12 @@
 
         (testing "and regenerate the tree"
           (let [new-v-tree (vt/update-children no-gen-v-tree [])
-                new-expected-vals (reset! expected-vals
-                                          {:props {:e "eff" :g "eych"}
-                                           :children [new-v-tree]
-                                           :local-state {:new "state"}})
-                regenerated-gen (g/generate (g/generator
-                                              gen-desc {:e "eff" :g "eych"}
-                                              (:children new-expected-vals))
-                                            generated-gen)]
+                _ (reset! expected-vals* {:props {:e "eff" :g "eych"}
+                                         :local-state {:new "state"}
+                                         :v-tree (vt/node classes/one-fish-desc)})
+                regenerated-gen (g/generate
+                                  (g/update-props generated-gen
+                                                  {:e "eff" :g "eych"}))]
             (is (= new-v-tree (g/v-tree regenerated-gen)))
             (is (identical? orig-head& (g/obj& regenerated-gen)))
             (is (= 0 (count (.getChildren orig-head&))))
